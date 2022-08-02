3b1f03325f0865eec729bcbcf9a6b494fe1a6a91
Yurham Afif <yurhamafif.edu@gmail.com>
2022-08-02 07:39:06 +0700

    Product with search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import data from './products.json'
+// import ClickCounter from './component/click_counter';
+// import HoverCounter from './component/hover_counter';
+import HelloComponent from './component/HelloComponent';
+import ProductCard from './features/product/ProductCard';
+import ProductList from './features/product/ProductList';
+
+const filterProducts = (searchTerm) => {
+  searchTerm = searchTerm.toUpperCase()
+  return data.products.filter(product => {
+    let str = `${product.productId} ${product.productName}`.toUpperCase();
+    return str.indexOf(searchTerm) >= 0;
+  })
+}
+
+const WithSearch = WrappedComponent => {
+  class WithSearch extends Component{
+    state = {
+      searchTerm: ""
+    };
+    handleSearch = event => {
+      this.setState({ searchTerm: event.target.value });
+    };
+
+    render() {
+      let { searchTerm } = this.state;
+      let filteredProducts = filterProducts(searchTerm);
+      return (
+          <>
+            <input
+              onChange={this.handleSearch}
+              value={searchTerm}
+              type="text"
+              placeholder="Search"
+            />
+            <WrappedComponent data={filteredProducts} />
+          </>
+      );
+    }
+  };
+  WithSearch.displayName = `WithSearch(${getDisplayName(WrappedComponent)})`;
+  return WithSearch;
+};
+
+const getDisplayName = (WrappedComponent) => {
+  return WrappedComponent.displayName || WrappedComponent.productName || 'Component';
+}
+
+const ProductsListWithSearch = WithSearch(ProductList);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <ClickCounter/> */}
+      {/* <HoverCounter/> */}
+      {/* <ProductCard data={data.products}/> */}
+      {/* <ProductList data={data.products}/> */}
+      <ProductsListWithSearch/>
+
     </div>
   );
 }
