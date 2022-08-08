7d555531bcc2c5ae24243c7f716c3eab51e17448
Yurham Afif <yurhamafif.edu@gmail.com>
2022-08-08 10:12:50 +0700

    [edit] use useEffect function dependency

--- src/components/MoreEffect.js
+++ src/components/MoreEffect.js
@@ -2,15 +2,22 @@ import { useEffect, useState } from "react";
 import moreEffectService from "../services/moreEffectService";
 
 const MoreEffect = () => {
-    const { doPrint } = moreEffectService();
-    const [result, setResult] = useState();
+    const {doPrint} = moreEffectService();
+    const [result, setResult] = useState('');
+    const [id, setId] = useState('10');
     // Allow a functional component to use lifecycle methods
+    // useEffect will be called when there's change in id
     useEffect(() => {
-        setResult(doPrint('1'));
+        setResult(doPrint(id));
         console.log('1');
-    },[])
+        // deps will control when to do useEffect
+    },[id])
     return(
         <>
+            <input type='text' value={id} onChange={(e) => {
+                setId(e.target.value)
+            }}></input>
+            {/* <button onClick={() => setId('10')}>Click</button> */}
             <h1>Print: {result}</h1>
         </>
     )
