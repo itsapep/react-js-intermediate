d1fc1a40a6c780092dff167dbfdc5c860190a9a7
Yurham Afif <yurhamafif.edu@gmail.com>
2022-08-08 09:29:10 +0700

    [add] create useState function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  //  allow a functional component to use a component-level state
+  const [dark, setDark] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor:dark?'black':'white'}}>
+      <p style={{color:dark?'white':'black'}}>Colour: {dark?'black':'white'}</p>
+      <button onClick={() => setDark(!dark)}>
+        set theme {dark? 'white':'black'}
+      </button>
     </div>
   );
 }
